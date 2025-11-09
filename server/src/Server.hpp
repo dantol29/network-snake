5cc285f321d3bc480275551431d33ec28a29c68f
Daniil Tolmacov <83882235+dantol29@users.noreply.github.com>
2025-11-09 11:24:13 +0200

    feat: stop server with stdin

--- server/src/Server.hpp
+++ server/src/Server.hpp
@@ -3,6 +3,7 @@
 
 #include "../includes/nibbler.hpp"
 #include "Game.hpp"
+#include "unistd.h"
 
 class Game;
 
@@ -21,6 +22,7 @@ private:
     int tcpServerFd;
     int udpServerFd;
     std::vector<struct pollfd> connectedClients;
+    std::vector<struct pollfd> newConnections;
     std::vector<int> closedConnections;
     std::unordered_map<in_addr_t, int> addressToFd;
     char readBuf[10];
@@ -29,14 +31,15 @@ private:
     std::string serializedHeight;
     std::string serializedWidth;
 
-    void setupSocket();
+    void setupSocket(int socket);
     void initConnections();
     void acceptNewConnection();
+    void addNewConnections();
     void closeConnection(const int fd);
     void removeClosedConnections();
     void sendGameData(const int fd) const;
-    void receiveDataFromClient(const int fd, const int index);
-    void handleSocketError(const int fd, const int index);
+    void receiveDataFromClient(const int fd);
+    void handleSocketError(const int fd);
     std::string serializeGameField();
 };
 
